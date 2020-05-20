b4f89683df9bb102f621d09e1d7b9f7df3e1d4f1
ST_Jiasiang.Ye 葉佳祥 <jiasiang.ye@ST-NB-2020005.local>
2020-05-20 10:34:56 +0800

    feat: add all logic .go

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	export "github.com/siangyeh8818/golang.exporter.TencentCloud/internal"
+
+)
+
+func main(){}
+	log.Println("Exporter is start ro running")
+
+	export.Run_Exporter_Server()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module siangyeh8818/golang.exporter.TencentCloud
+
+go 1.14
+
+require github.com/siangyeh8818/golang.exporter.templeate v0.0.0-20200519101836-58a5233574e1 // indirect
